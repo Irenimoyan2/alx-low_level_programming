5e81e41dbb3b2341571f4e40323f7f167e00d3a3
Irenimoyan2 <olanrewajuabibat@gmail.com>
2023-02-17 18:01:09 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,13 @@ int main(void)
 	int tens;
 	int ones;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print tens place*/
+	for (tens = '0'; tens <= '8'; tens++) /*print tens place*/
 	{
-		for (ones = '0'; ones <= '9'; ones++) /*print ones place*/
+		for (ones = '0'; ones <= '8'; ones++) /*print ones place*/
 		{
 			putchar(tens);
 			putchar(ones);
-			if (!(tens == '9' && ones == '9')) /*skip comma at end*/
+			if (!(tens == '8' && ones == '8')) /*skip comma at end*/
 			{
 				putchar(',');
 				putchar(' ');
