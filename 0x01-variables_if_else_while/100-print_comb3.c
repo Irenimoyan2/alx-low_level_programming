b13f520ee5ecf8774d4d388f4788c8b065923ad6
Irenimoyan2 <olanrewajuabibat@gmail.com>
2023-02-17 18:21:02 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
-
 /**
- *main - Entry point, print 00 to 99 using putchar
- *Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int tens;
-	int ones;
-
-	for (tens = '0'; tens <= '8'; tens++) /*print tens place*/
-	{
-		for (ones = '0'; ones <= '8'; ones++) /*print ones place*/
-		{
-			putchar(tens);
-			putchar(ones);
-			if (!(tens == '8' && ones == '8')) /*skip comma at end*/
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
-
-	return (0);
+int nums;
+int nums1;
+for (nums = '0'; nums < '9'; nums++)
+{
+for (nums1 = '0'; nums1 <= '9'; nums1++)
+{
+if (nums == '0' && nums1 < '1')
+continue;
+if (nums == '1' && nums1 < '2')
+continue;
+if (nums == '2' && nums1 < '3')
+continue;
+if (nums == '3' && nums1 < '4')
+continue;
+if (nums == '4' && nums1 < '5')
+continue;
+if (nums == '5' && nums1 < '6')
+continue;
+if (nums == '6' && nums1 < '7')
+continue;
+if (nums == '7' && nums1 < '8')
+continue;
+if (nums == '8' && nums1 < '9')
+continue;
+putchar(nums);
+putchar(nums1);
+if (nums == '8' && nums1 == '9')
+break;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
