0c21e82323c3f86736368e5174951fdf1576dee7
Irenimoyan2 <olanrewajuabibat@gmail.com>
2023-02-21 14:07:16 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,21 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - print Alx, followed by new line
- * Return: Always  0 (Success)
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	_putchar('A');
+	_putchar('H');
+	_putchar('o');
 	_putchar('l');
-	_putchar('x');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
