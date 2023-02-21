20648e222ec8863574d067f7d6cd6b254109db48
Irenimoyan2 <olanrewajuabibat@gmail.com>
2023-02-21 13:56:54 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "Alx.h"
+
+/**
+ * main - print Alx, followed by new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('A');
+	_putchar('l');
+	_putchar('x');
+	_putchar('\n');
+
+	return (0);
+}
