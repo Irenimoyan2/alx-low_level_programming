82f897dfa94f0ba41bb53977ae3f9bf0af77c41b
Irenimoyan2 <olanrewajuabibat@gmail.com>
2023-02-23 16:05:08 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,14 +7,12 @@
  * Return: Always or 1
  */
 
-int _isupper(int c);
+int _isupper(int c)
 {
-	if (c >= '65' && c <= '90')
-	{
+	if (c >= 'A' && c <= 'Z')
+	
 		return (1);
-	}
+	
 	else
-	{
 		return (0);
-	}
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-int _putchar.c(char c);
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
