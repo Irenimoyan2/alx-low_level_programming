c648cd7589db7453f8109e7256ba93edbb3f3d36
Irenimoyan2 <olanrewajuabibat@gmail.com>
2023-02-23 16:47:57 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - return de value of multiplication between a and b
+ * @a: input one
+ * @b: input two
+ * Return: the respective value of multiplication
+ */
+int mul(int a, int b)
+{
+long int mul;
+mul = a * b;
+return (mul);
+}
