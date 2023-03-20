f666b1011b2c18a057044d778d219f2e6ef1fa31
Irenimoyan2 <olanrewajuabibat@gmail.com>
2023-03-20 16:40:00 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * main - Entry point
+ * Descr: Header file that defines a function-like macro
+ * SUM(x, y), that computes
+ * @x: sum of the numbers
+ * @y: the sum of macro
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
