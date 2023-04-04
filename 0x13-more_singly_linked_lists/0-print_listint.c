2af18c2d60450c0f44860b1ce53d986d707ca749
Irenimoyan2 <olanrewajuabibat@gmail.com>
2023-04-04 03:41:42 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,11 @@
-#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include "lists.h"
 
 /**
- * print_listint - Function that prints all the elements of a listint_t list
+ * print_listint - Entry point
+ * Description: Function that prints all the elements of a listint_t list
  * @h: pointer to the listint_t
  *
  * Return: Number of nodes in list
