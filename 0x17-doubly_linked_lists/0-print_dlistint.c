ac60da4c14b72ce674032e9d3ab03a512a54a9c7
Irenimoyan2 <olanrewajuabibat@gmail.com>
2023-05-22 10:29:30 -0700

    doubel linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -6,7 +9,6 @@
  * @h: Pointer to the list.
  * Return: number of nodes.
  **/
-
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *node = h;
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - Entry Point
+ * Description: function that adds a new node at the beginning of a dlistint_t list
+ * @head: pointer to the first element of the list
+ * @n: int to set in the new node.
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	/* Allocate memory for a new node */
+       	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	/* Assign the value 'n' to the 'n' member of the new node */
+	new_node->n;
+
+	/* Update the prev and next pointers of the new node*/
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	/* update prev pointer of the old head node if it exists*/
+	if (*head != NULL);
+	{
+		(*head)->prev = new_node;
+	}
+
+	/* update the head pointer to point to the new node */
+	*head = new_node;
+
+	return (new_node);
+}
