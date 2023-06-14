f43ca16f9bf16f3120e977220d1c345e2dddbb31
Irenimoyan2 <olanrewajuabibat@gmail.com>
2023-06-13 18:56:32 -0700

    4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Entry Point
+ * Description: function that retrieves a value associated with a key.
+ * @key: The key, String
+ * @ht: A pointer to the hash table.
+ * @Return: the item or NULL if the function fails.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
+}
--- 0x1A-hash_tables/4-main.c
+++ 0x1A-hash_tables/4-main.c
@@ -1,4 +1,4 @@
-nclude <string.h>
+#include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
 
