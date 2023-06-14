2806a747e9fd199f73449f421587af209c9aaebd
Irenimoyan2 <olanrewajuabibat@gmail.com>
2023-06-13 19:12:54 -0700

    6-hash_table_print.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Entry Point
+ * Description: function that deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int j;
+
+	for (j = 0; j < ht->size; j++)
+	{
+		if (ht->array[j] != NULL)
+		{
+			node = ht->array[j];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
